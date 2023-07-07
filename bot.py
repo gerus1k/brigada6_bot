c51e69722f31446cf8257ceb98bd0b58bff933d2
justUzm <90555971+justUzm@users.noreply.github.com>
2023-07-07 13:27:08 +0300

    added processing of the selected language and implemented the output of function buttons based on it

--- bot.py
+++ bot.py
@@ -7,14 +7,39 @@ bot = telebot.TeleBot('6022109518:AAHgixLTjERH8Caogmg_lsz1v2y5D38iNIg')
 # Выбор языка при вызове /start
 @bot.message_handler(commands=['start'])
 def start(message):
-    # Создаем клавиатуру с кнопками выбора языка
     markup = types.InlineKeyboardMarkup()
     russian_button = types.InlineKeyboardButton('🇷🇺Русский', callback_data='ru')
     english_button = types.InlineKeyboardButton('🇬🇧English', callback_data='en')
     markup.add(russian_button, english_button)
 
-    # Отправляем сообщение с кнопками выбора языка
     bot.send_message(message.chat.id, 'Выберите язык / Choose language:', reply_markup=markup)
+
+# Выводим основной интерфейс бота в зависимости от выбранного языка
+@bot.callback_query_handler(func=lambda call: True)
+def button(call):
+    language = call.data
+
+    if language == 'ru':
+        markup = types.InlineKeyboardMarkup()
+        translator_button = types.InlineKeyboardButton('Переводчик', callback_data='translator')
+        calculator_button = types.InlineKeyboardButton('Калькулятор', callback_data='calculator')
+        weather_button = types.InlineKeyboardButton('Погода', callback_data='weather')
+        meme_button = types.InlineKeyboardButton('Мем дня', callback_data='meme')
+        markup.add(translator_button, calculator_button, weather_button, meme_button)
+
+        bot.send_message(call.message.chat.id, 'Выберите функцию:', reply_markup=markup)
+    elif language == 'en':
+        markup = types.InlineKeyboardMarkup()
+        translator_button = types.InlineKeyboardButton('Translator', callback_data='translator')
+        calculator_button = types.InlineKeyboardButton('Calculator', callback_data='calculator')
+        weather_button = types.InlineKeyboardButton('Weather', callback_data='weather')
+        meme_button = types.InlineKeyboardButton('Meme', callback_data='meme')
+        markup.add(translator_button, calculator_button, weather_button, meme_button)
+
+        bot.send_message(call.message.chat.id, 'Select function:', reply_markup=markup)
+
+    bot.answer_callback_query(call.id)
+
 
 
 
